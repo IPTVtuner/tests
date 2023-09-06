3978456c5b91bdbf429fff8ef9ac915559907276
IPTVtuner <68898576+IPTVtuner@users.noreply.github.com>
2023-09-06 10:58:31 +0100

    Update and rename franceinfo.web to capture.py

---
+++ capture.py
@@ -0,0 +1,29 @@
+import requests
+import json
+import os
+
+def getTime():
+    pass
+
+
+if __name__ == '__main__':
+    with open('channelList.json') as channelList:
+        channels = json.load(channelList)
+    #channels = {'Sky News': 'sky-news', 'Tiny Pop (Freeview) Today': 'tiny-pop-freeview'}
+    baseurl = 'https://tv24.co.uk'
+    for channel in channels:
+        ch_name = channels[channel]
+        link, title, description = 'NA', 'NA', 'NA'
+        try:
+            response = requests.get(f'{baseurl}/x/channel/{ch_name}/330/today').text
+            raw = response.split('<li class="ongoing">')[-1].split('</li>')[0]
+            link = baseurl + raw.split('<a href="')[-1].split('"')[0]
+            title = raw.split('<h3>')[-1].split('</h3>')[0]
+            description = raw.split('<p>')[-1].split('</p>')[0]
+        except Exception as e:
+            print(f'some error: {e}')
+        if 'files' not in os.listdir():
+            os.mkdir('files')
+        with open(f'files/{ch_name}.txt', 'w') as writer:
+            writer.write(f'{description}')
+        print(f'{channel}\t{description}')
